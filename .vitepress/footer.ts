58dd96e2a8ea7bdc9cb1f2353c7963eb5f92a689
novlan1 <1576271227@qq.com>
2025-04-26 09:42:14 +0800

    feat: update footer

--- .vitepress/footer.ts
+++ .vitepress/footer.ts
@@ -15,6 +15,18 @@ const MESSAGE_LIST = [
     text: 'ONE',
     link: 'https://novlan1.github.io/one/',
   },
+  {
+    text: 'GAME',
+    link: 'https://novlan1.github.io/canvas-game/',
+  },
+  {
+    text: 'Comment',
+    link: 'https://novlan1.github.io/comment-code-online/',
+  },
+  {
+    text: 'More CSS',
+    link: 'https://novlan1.github.io/more-css/',
+  }
 ];
 
 export function getFooterMessage() {
