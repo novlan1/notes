094db6979c3629dcef56572468b7feab08f8480f
novlan1 <1576271227@qq.com>
2025-12-08 11:19:45 +0000

    Deploying to docs from  @ 2563f61ea278216a7757f19daa9dadf8705795f6 🚀

--- assets/chunks/clone.DtdMAOGg.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./baseUniq.DeAq7n3S.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ assets/docs_2025下_whistle-config.md.CQCRNmcG.js
@@ -0,0 +1,44 @@
+import{_ as a,c as i,o as n,a2 as t}from"./chunks/framework.CriqKoQt.js";const g=JSON.parse('{"title":"whistle 配置文件","description":"","frontmatter":{},"headers":[],"relativePath":"docs/2025下/whistle-config.md","filePath":"docs/2025下/whistle-config.md","lastUpdated":1765192670000}'),p={name:"docs/2025下/whistle-config.md"};function l(e,s,h,k,c,F){return n(),i("div",null,s[0]||(s[0]=[t(`<h1 id="whistle-配置文件" tabindex="-1">whistle 配置文件 <a class="header-anchor" href="#whistle-配置文件" aria-label="Permalink to &quot;whistle 配置文件&quot;">​</a></h1><div class="language-sh vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:443</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test.igame.qq.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:443</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-443.nes.smoba.qq.com</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># wss://h5-test.igame.qq.com wss://127.0.0.1:443</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:444</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-444.igame.qq.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:445</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-445.igame.qq.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:446</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-446.igame.qq.com</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:610</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://match-3-test.igame.qq.com</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:701</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-701.igame.qq.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:778</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://tgs.nes.sgameglobal.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:778</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://test.partner.esports.pubgmobile.com</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:852</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://test.esports.tencent.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:883</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test.dfpcevos.igame.tencent.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:899</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test.nes.sgameglobal.com</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:989</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://989.h5-test.nes.smoba.qq.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:989</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://xd-test.woa.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:999</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-999.igame.qq.com</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:1006</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">  https://h5-test-1006.igame.qq.com</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">127.0.0.1:8080</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> https://h5-pro-test.nes.smoba.qq.com</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">20.205.243.166</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> github.com</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">140.82.113.3</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> github.com</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># https://api-test.igame.qq.com/merc.plugin.commercial_cgi.commercial_cgi/GetRedeemCode resBody://{GetRedeemCodeSucc}</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># https://image-1251917893.file.myqcloud.com/igame/npm/vconsole-helper/index%400.0.4.prod.js file:///Users/guowangyang/Documents/git-woa/tx-plugin-light/packages/vconsole-helper/lib/index.prod.js</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># https://image-1251917893.file.myqcloud.com/igame/npm/vconsole-helper/index@0.0.4.prod.js?t=1313123</span></span></code></pre></div>`,2)]))}const d=a(p,[["render",l]]);export{g as __pageData,d as default};
